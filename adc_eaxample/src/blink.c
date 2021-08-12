aa93c08462b562913abad06f041a5f8a6d10d6c8
sjpark608 <spark@osensa.com>
2021-08-11 18:23:18 -0700

    Scan mode tested
    Debug optimization enabled
    
    Scan result updated in debug mode

--- adc_eaxample/src/blink.c
+++ adc_eaxample/src/blink.c
@@ -35,7 +35,7 @@ void ADC0_IRQHandler(void)
 	uint32_t status = 0;
 	uint32_t valid = 0;
 	ADC0->IFC = ADC_IFC_SCAN;
-	status = (ADC0->STATUS)>>24;
+	status = (((ADC0->STATUS)& _ADC_STATUS_SCANDATASRC_MASK)>>24);
 	valid = (ADC0->STATUS) & ADC_STATUS_SCANDV;
 	if(ADC0->STATUS & ADC_STATUS_SCANDV)
 	{
@@ -50,7 +50,9 @@ void ADC0_IRQHandler(void)
 				adc_set = true;
 				break;
 		}
+
 	}
+	GPIO_PinOutToggle(gpioPortC, 1);
 
 //	ADC0 -> IFC = ADC_IFC_SINGLE;
 //	adcReading0 = ADC_DataSingleGet(ADC0);
@@ -129,7 +131,6 @@ int main(void)
  		if(adc_set)
  		{
  			adc_set = false;
- 			GPIO_PinOutToggle(gpioPortC, 1);
  		}
  	}
 }
@@ -140,7 +141,7 @@ void timer_config(void)
 {
 	TIMER_Init_TypeDef timerInit = TIMER_INIT_DEFAULT;
 //	timerInit.prescale = timerPrescale64;	//HFPERCLK undivided
-	TIMER_TopSet(TIMER0, 1400); //this will make the LOG_SIG ADC run at 25MHz/25000 = ~1kHz
+	TIMER_TopSet(TIMER0, 25000); //this will make the LOG_SIG ADC run at 25MHz/25000 = ~1kHz
 	TIMER_Init(TIMER0, &timerInit);
 }
 
@@ -149,23 +150,15 @@ void adc_config(void)
 
 	// Base the ADC configuration on the default setup.
 	ADC_Init_TypeDef       init       = ADC_INIT_DEFAULT;
-	ADC_InitSingle_TypeDef singleInit = ADC_INITSINGLE_DEFAULT;
 	ADC_InitScan_TypeDef  init_scan = ADC_INITSCAN_DEFAULT;
 
 	// Initialize timebases
 	init.timebase = ADC_TimebaseCalc(0);
-	init.prescale = ADC_PrescaleCalc(7000000,0);
+	init.prescale = ADC_PrescaleCalc(10000,0);
 	ADC_Init(ADC0, &init);
 
-
-	// Set up scan initialization channel 0 1 2, enable PRS on PRS_ch 0. Timer0 will trigger the scan
-//	init_scan.reference = adcRef2V5;
-//	init_scan.input = ADC_SCANCTRL_INPUTMASK_CH0 | ADC_SCANCTRL_INPUTMASK_CH1;
-//	init_scan.prsEnable  = true; // Enable PRS for ADC
-//	ADC_InitScan(ADC0, &init_scan);
-
 	init_scan.reference = adcRef2V5;
-	init_scan.input = ADC_SCANCTRL_INPUTMASK_CH0 | ADC_SCANCTRL_INPUTMASK_CH1;
+	init_scan.input = ADC_SCANCTRL_INPUTMASK_CH0 | ADC_SCANCTRL_INPUTMASK_CH1 | ADC_SCANCTRL_INPUTMASK_CH2;
 	init_scan.resolution = adcRes12Bit;
 	init_scan.diff = false;
 	init_scan.prsEnable = true;
@@ -173,7 +166,6 @@ void adc_config(void)
 	// Enable ADC Interrupt when Single and SCAN conversion Complete
 	ADC0->IEN = ADC_IEN_SCAN;
 //	ADC0->IEN = ADC_IEN_SINGLE;
-
 	// Enable ADC interrupt vector in NVIC
 	NVIC_EnableIRQ(ADC0_IRQn);
 	NVIC_SetPriority(ADC0_IRQn, 1);  // highest pre-empt priority
